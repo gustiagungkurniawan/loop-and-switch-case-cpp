f1ce90eeece693e053b39e32d5aa09269be45c01
Gusti Agung Kurniawan <4gustiagung@gmail.com>
2025-05-09 17:51:05 +0800

    Input nama panggilan lalu tampil

---
+++ loop-case-break.cpp
@@ -0,0 +1,271 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main(){
+	string nama;
+	int i;
+	
+	cout<<"$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$" <<endl;
+	cout<<"@@@@@@@@@@ Code By Gusti Agung Kurniawan @@@@@@@@@@@" <<endl;
+	cout<<"$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$" <<endl;
+	cout<<"$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$" <<endl;
+	cout<<"$$$$$$      $$ $$$$$ $$      $$       $$       $$$$$" <<endl;
+	cout<<"$$$$$$ $$$$$$$ $$$$$ $$ $$$$$$$$$$ $$$$$$$$ $$$$$$$$" <<endl;
+	cout<<"$$$$$$ $$$$$$$ $$$$$ $$      $$$$$ $$$$$$$$ $$$$$$$$" <<endl;
+	cout<<"$$$$$$ $$$  $$ $$$$$ $$$$$$$ $$$$$ $$$$$$$$ $$$$$$$$" <<endl;
+	cout<<"$$$$$$ $$$$ $$ $$$$$ $$$$$$$ $$$$$ $$$$$$$$ $$$$$$$$" <<endl;
+	cout<<"$$$$$$      $$       $$      $$$$$ $$$$$       $$$$$" <<endl;
+	cout<<"$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$" <<endl;
+	cout<<"$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$" <<endl<<endl;
+	
+	cout<<"********************* Gus TI ***********************" <<endl <<endl;
+
+	cout<<"Nama panggilanmu siapa:) ?"<<endl;
+	cout<<"Jawab : ";
+	cin>>nama;
+	cout<<endl<<endl;
+	
+	i = nama.length();
+	
+	for(int a=0; a<i; a++){
+		switch(nama[a]){
+			case 'a':
+			case 'A':
+				cout<<"...$$$$..."<<endl;
+				cout<<"..$....$.."<<endl;
+				cout<<"..$$$$$$.."<<endl;
+				cout<<"..$....$.."<<endl;
+				cout<<"..$....$.."<<endl<<endl;
+			
+			break;
+			
+			case 'b':
+			case 'B':
+				cout<<"..$$$$$$.."<<endl;
+				cout<<"..$....$.."<<endl;
+				cout<<"..$$$$$..."<<endl;
+				cout<<"..$....$.."<<endl;
+				cout<<"..$$$$$$.."<<endl<<endl;
+			break;
+			
+			case 'c':
+			case 'C':
+				cout<<"..$$$$$$.."<<endl;
+				cout<<"..$......."<<endl;
+				cout<<"..$......."<<endl;
+				cout<<"..$......."<<endl;
+				cout<<"..$$$$$$.."<<endl<<endl;
+			break;
+			
+			case 'd':
+			case 'D':
+				cout<<"..$$$$$..."<<endl;
+				cout<<"..$....$.."<<endl;
+				cout<<"..$....$.."<<endl;
+				cout<<"..$....$.."<<endl;
+				cout<<"..$$$$$..."<<endl<<endl;
+			break;
+			
+			case 'e':
+			case 'E':
+				cout<<"..$$$$$$.."<<endl;
+				cout<<"..$......."<<endl;
+				cout<<"..$$$$$$.."<<endl;
+				cout<<"..$......."<<endl;
+				cout<<"..$$$$$$.."<<endl<<endl;
+			break;
+			
+			case 'f':
+			case 'F':
+				cout<<"..$$$$$$.."<<endl;
+				cout<<"..$......."<<endl;
+				cout<<"..$$$$...."<<endl;
+				cout<<"..$......."<<endl;
+				cout<<"..$......."<<endl<<endl;
+			break;
+			
+			case 'g':
+			case 'G':
+				cout<<"..$$$$$$.."<<endl;
+				cout<<"..$......."<<endl;
+				cout<<"..$..$$$.."<<endl;
+				cout<<"..$....$.."<<endl;
+				cout<<"..$$$$$$.."<<endl<<endl;
+			break;
+			
+			case 'h':
+			case 'H':
+				cout<<"..$....$.."<<endl;
+				cout<<"..$....$.."<<endl;
+				cout<<"..$$$$$$.."<<endl;
+				cout<<"..$....$.."<<endl;
+				cout<<"..$....$.."<<endl<<endl;
+			break;
+			
+			case 'i':
+			case 'I':
+				cout<<"..$$$$$..."<<endl;
+				cout<<"....$....."<<endl;
+				cout<<"....$....."<<endl;
+				cout<<"....$....."<<endl;
+				cout<<"..$$$$$..."<<endl<<endl;
+			break;
+			
+			case 'j':
+			case 'J':
+				cout<<"..$$$$$$.."<<endl;
+				cout<<".......$.."<<endl;
+				cout<<".......$.."<<endl;
+				cout<<"..$....$.."<<endl;
+				cout<<"...$$$$..."<<endl<<endl;
+			break;
+			
+			case 'k':
+			case 'K':
+				cout<<"..$....$.."<<endl;
+				cout<<"..$...$..."<<endl;
+				cout<<"..$$$$...."<<endl;
+				cout<<"..$...$..."<<endl;
+				cout<<"..$....$.."<<endl<<endl;
+			break;
+			
+			case 'l':
+			case 'L':
+				cout<<"..$......."<<endl;
+				cout<<"..$......."<<endl;
+				cout<<"..$......."<<endl;
+				cout<<"..$......."<<endl;
+				cout<<"..$$$$$$.."<<endl<<endl;
+			break;
+			
+			case 'm':
+			case 'M':
+				cout<<"..$$...$$.."<<endl;
+				cout<<"..$.$.$.$.."<<endl;
+				cout<<"..$..$..$.."<<endl;
+				cout<<"..$.....$.."<<endl;
+				cout<<"..$.....$.."<<endl<<endl;
+			break;
+			
+			case 'n':
+			case 'N':
+				cout<<"..$....$.."<<endl;
+				cout<<"..$$...$.."<<endl;
+				cout<<"..$.$..$.."<<endl;
+				cout<<"..$..$.$.."<<endl;
+				cout<<"..$...$$.."<<endl<<endl;
+			break;
+			
+			case 'o':
+			case 'O':
+				cout<<"...$$$$..."<<endl;
+				cout<<"..$....$.."<<endl;
+				cout<<"..$....$.."<<endl;
+				cout<<"..$....$.."<<endl;
+				cout<<"...$$$$..."<<endl<<endl;
+			break;
+			
+			case 'p':
+			case 'P':
+				cout<<"..$$$$$..."<<endl;
+				cout<<"..$....$.."<<endl;
+				cout<<"..$$$$$..."<<endl;
+				cout<<"..$......."<<endl;
+				cout<<"..$......."<<endl<<endl;
+			break;
+			
+			case 'q':
+			case 'Q':
+				cout<<"...$$$$..."<<endl;
+				cout<<"..$....$.."<<endl;
+				cout<<"..$..$.$.."<<endl;
+				cout<<"..$...$$.."<<endl;
+				cout<<"...$$$$$.."<<endl;
+				cout<<"........$."<<endl<<endl;
+			break;
+			
+			case 'r':
+			case 'R':
+				cout<<"..$$$$$..."<<endl;
+				cout<<"..$....$.."<<endl;
+				cout<<"..$$$$$..."<<endl;
+				cout<<"..$...$..."<<endl;
+				cout<<"..$....$.."<<endl<<endl;
+			break;
+			
+			case 's':
+			case 'S':
+				cout<<"...$$$$$.."<<endl;
+				cout<<"..$......."<<endl;
+				cout<<"..$$$$$$.."<<endl;
+				cout<<".......$.."<<endl;
+				cout<<"..$$$$$..."<<endl<<endl;
+			break;
+			
+			case 't':
+			case 'T':
+				cout<<".$$$$$$$.."<<endl;
+				cout<<"....$....."<<endl;
+				cout<<"....$....."<<endl;
+				cout<<"....$....."<<endl;
+				cout<<"....$....."<<endl<<endl;
+			break;
+			
+			case 'u':
+			case 'U':
+				cout<<"..$....$.."<<endl;
+				cout<<"..$....$.."<<endl;
+				cout<<"..$....$.."<<endl;
+				cout<<"..$....$.."<<endl;
+				cout<<"...$$$$..."<<endl<<endl;
+			break;
+			
+			case 'v':
+			case 'V':
+				cout<<"..$....$.."<<endl;
+				cout<<"..$....$.."<<endl;
+				cout<<"..$....$.."<<endl;
+				cout<<"...$..$..."<<endl;
+				cout<<"....$$...."<<endl<<endl;
+			break;
+			
+			case 'w':
+			case 'W':
+				cout<<"..$.....$."<<endl;
+				cout<<"..$.....$."<<endl;
+				cout<<"..$..$..$."<<endl;
+				cout<<"..$.$.$.$."<<endl;
+				cout<<"..$$$.$$$."<<endl<<endl;
+			break;
+			
+			case 'x':
+			case 'X':
+				cout<<"..$$...$$."<<endl;
+				cout<<"....$.$..."<<endl;
+				cout<<".....$...."<<endl;
+				cout<<"....$.$..."<<endl;
+				cout<<"..$$...$$."<<endl<<endl;
+			break;
+			
+			case 'y':
+			case 'Y':
+				cout<<"..$...$..."<<endl;
+				cout<<"..$...$..."<<endl;
+				cout<<"...$.$...."<<endl;
+				cout<<"....$....."<<endl;
+				cout<<"....$....."<<endl<<endl;
+			break;
+			
+			case 'z':
+			case 'Z':
+				cout<<"..$$$$$$.."<<endl;
+				cout<<"......$..."<<endl;
+				cout<<"....$$...."<<endl;
+				cout<<"...$......"<<endl;
+				cout<<"..$$$$$$.."<<endl<<endl;
+			break;
+		}
+		
+	}
+}
